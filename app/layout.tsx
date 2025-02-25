0b6cf848e7e8102946b0259e1fc807edac75407e
Ahmad Lazim <ahmadlazim422@gmail.com>
2025-02-25 22:48:31 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,23 +2,12 @@
 import type { Metadata } from "next";
 import Head from "next/head";
 import Script from "next/script";
-import { Geist, Geist_Mono } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/main.css";
 import "./assets/vendor/bootstrap-icons/bootstrap-icons.css";
 import "./assets/vendor/aos/aos.css";
 import "./assets/vendor/glightbox/css/glightbox.min.css";
 
-// Gunakan font dengan lebih efisien
-const geistSans = Geist({
-  subsets: ["latin"],
-  variable: "--font-geist-sans",
-});
-
-const geistMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-geist-mono",
-});
 
 export const metadata: Metadata = {
   title: "Landing Page",
@@ -44,7 +33,7 @@ export default function RootLayout({
 
         
       </Head>
-      <body className={`index-page ${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`index-page antialiased`}>
         {children}
         <Script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js" strategy="beforeInteractive" />
         <Script src="/assets/vendor/php-email-form/validate.js" strategy="lazyOnload" />
